1c952a084c2bc5cf44a872f3ad9ce2ed03752c41
33 <tommaldonado444@gmail.com>
2023-03-16 16:50:37 -0300

    Concluimos o calculo de idade utilizando a linguagem Python.

---
+++ python.py
@@ -0,0 +1,8 @@
+Nascimento = 1997
+AnoAtual = 2023
+Nome = 'Caio'
+def CalculoIdade(Nascimento, AnoAtual):
+    idade = AnoAtual - Nascimento
+    return idade
+idade = CalculoIdade(Nascimento, AnoAtual)
+print(idade)
